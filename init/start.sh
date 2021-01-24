2d0de881fd317d383377d8a3d302dcb884b04280
Furuhashii <64199601+Furuhashii@users.noreply.github.com>
2021-01-25 00:25:07 +0700

    Add files via upload

---
+++ init/start.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+# Copyright (C) 2019 The Edelweiss Groups Company LLC.
+#
+# Licensed under the Edelweiss Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+redis-server --daemonize yes
+python3 -m userbot
+
